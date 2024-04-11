783a61ac615bb3ccefe01096c40d8e0653752015
oddy-bassey <oddybassey@gmail.com>
2024-04-11 22:06:52 +0100

    implemented code to solve Block Constructor problem

---
+++ src/coderbyte/blockConstructor/BlockConstructor.java
@@ -0,0 +1,131 @@
+package coderbyte.blockConstructor;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.util.*;
+
+public class BlockConstructor {
+    public static void main (String[] args) {
+        /* create varriables to hold the following data
+         *
+         * mempool.csv file path
+         * maxWeight
+         * read map of transactions
+         * list of sorted txIds
+         * list of selected txIds
+         *
+         */
+        String filePath = "mempool.csv";
+        final int maxWeight = 4000000;
+
+        try{
+            Map<String, Transaction> transactions = parseFile(filePath);
+            List<String> sortedTxIds = sortTransactions(transactions);
+            List<String> selectedTxIds = selectTransactions(transactions, sortedTxIds, maxWeight);
+
+            for(String txid : selectedTxIds) {
+                System.out.println(txid);
+            }
+        }catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // Transaction class object to hold data
+    static class Transaction {
+        private String txid;
+        private Integer fee;
+        private Integer weight;
+        private List<String> parentTxids;
+
+        public Transaction(String txid, Integer fee, Integer weight, List<String> parentTxids) {
+            this.txid = txid;
+            this.fee = fee;
+            this.weight = weight;
+            this.parentTxids = parentTxids;
+        }
+    }
+
+    // method to parse mempool.csv file
+    public static Map<String, Transaction> parseFile(String filePath) throws IOException {
+        Map<String, Transaction> transactions = new HashMap<>();
+
+        // read the file into a BufferedStream object
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line;
+
+        // read each line in the stream until its null
+        while((line = reader.readLine()) != null) {
+            String[] data = line.split(",");
+            String txId = data[0];
+            Integer fee = Integer.parseInt(data[1]);
+            Integer weight = Integer.parseInt((data[2]));
+
+            // check if the transacton has parents
+            List<String> parents = new ArrayList<>();
+            if(data.length > 3){
+                parents = Arrays.asList(data[4].split(";"));
+            }
+            // add the read transaction to the map data
+            transactions.put(txId, new Transaction(txId, fee, weight, parents));
+        }
+        reader.close();
+        return transactions;
+    }
+
+    // create method to sort the transactions
+    public static List<String> sortTransactions(Map<String, Transaction> transactions) {
+        Map<String, Integer> txDegree = new HashMap<>();
+        Map<String, List<String>> txGraph = new HashMap<>();
+
+        // Initialize the transaction occurrence/degree and graph map
+        for (Transaction transaction : transactions.values()) {
+            txDegree.put(transaction.txid, 0);
+            txGraph.put(transaction.txid, new ArrayList<>());
+        }
+
+        // Construct the transaction graph and calculate the number of occurrence/degrees
+        for (Transaction transaction : transactions.values()) {
+            for (String parent : transaction.parentTxids) {
+                txGraph.get(parent).add(transaction.txid);
+                txDegree.put(transaction.txid, txDegree.get(transaction.txid) + 1);
+            }
+        }
+
+        // sort the transactions
+        List<String> result = new ArrayList<>();
+        Queue<String> queue = new LinkedList<>();
+        for (Map.Entry<String, Integer> entry : txDegree.entrySet()) {
+            if (entry.getValue() == 0) {
+                queue.offer(entry.getKey());
+            }
+        }
+        while (!queue.isEmpty()) {
+            String current = queue.poll();
+            result.add(current);
+            for (String child : txGraph.get(current)) {
+                txDegree.put(child, txDegree.get(child) - 1);
+                if (txDegree.get(child) == 0) {
+                    queue.offer(child);
+                }
+            }
+        }
+        return result;
+    }
+
+    // create method to select the transactions
+    public static List<String> selectTransactions(Map<String, Transaction> transactions, List<String> sortedTxids, int maxWeight) {
+        List<String> selectedTxids = new ArrayList<>();
+        int totalWeight = 0;
+        for(String txid : sortedTxids) {
+            if (totalWeight + transactions.get(txid).weight <= maxWeight) {
+                selectedTxids.add(txid);
+                totalWeight += transactions.get(txid).weight;
+            }
+        }
+
+        return selectedTxids;
+    }
+}
+
+
--- src/hackerrank/fetchItemsToDisplay/FetchItemsToDisplay.java
+++ src/hackerrank/fetchItemsToDisplay/FetchItemsToDisplay.java
@@ -1,6 +1,80 @@
 package hackerrank.fetchItemsToDisplay;
 
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class FetchItemsToDisplay {
 
 
+    /*
+     * Complete the 'fetchItemsToDisplay' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. 2D_STRING_ARRAY items
+     *  2. INTEGER sortParameter
+     *  3. INTEGER sortOrder
+     *  4. INTEGER X
+     */
+
+    public static List<String> fetchItemsToDisplay(List<List<String>> items, int sortParameter, int sortOrder, int X) {
+        List<String> result = new ArrayList<>();
+
+        if(items.isEmpty()) return result;
+        if(items.get(0).get(sortParameter) == null) return result;
+
+        items.stream()
+                .map(item -> item.get(sortParameter))
+                .sorted(sortOrder == 0? Comparator.naturalOrder() : Comparator.reverseOrder())
+                .collect(Collectors.toList()).forEach(item -> {
+                    if(result.size() < X) {
+                        result.add(item);
+                    }
+                });
+
+        return result;
+    }
+
+    public static int palindrome(String s) {
+        char[] characters = s.toCharArray();
+        int numberOfPalins = 0;
+        Map<String, String> charSet = new HashMap();
+
+        for (int i = 0; i < characters.length; i++) {
+            StringBuilder word = new StringBuilder(characters[i]);
+
+            if(charSet.get(word.toString()) == null) {
+                if(isPalindrome(word.toString())) numberOfPalins++;
+                charSet.put(word.toString(), word.toString());
+            }
+
+            for (int j = i + 1; j < characters.length; j++) {
+                word.append(characters[j]);
+                if (isPalindrome(word.toString())) numberOfPalins++;
+            }
+        }
+
+        return numberOfPalins;
+    }
+
+    public static boolean isPalindrome(String str)
+    {
+        String rev = "";
+        boolean ans = false;
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        if (str.equals(rev)) {
+            ans = true;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+       // minimalOperations().forEach(System.out::println);
+        System.out.println(palindrome("mokkori"));
+    }
 }
